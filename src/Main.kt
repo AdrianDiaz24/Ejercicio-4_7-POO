f19f48d05faf7d136233df79145cf85dbadd2223
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-01-31 20:39:28 +0100

    31/01

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,12 @@
 
 
 fun main(){
-    val persona1 = Persona("32939158F", arrayOf(Cuenta(45), null, null, null))
+    val cuenta1 = Cuenta(1)
+    val cuenta2 = Cuenta(2, 700.0)
+    val persona1 = Persona("32939158F", arrayOf(cuenta1,cuenta2 ))
+    persona1.cuentas[0]?.recibirAbono(1100.0)
+    persona1.cuentas[1]?.realizarPago(750.0)
+    persona1.esMorosa()
+    persona1.transferencia(persona1,persona1,0,1,200.0)
+    persona1.esMorosa()
 }
--- src/Persona.kt
+++ src/Persona.kt
@@ -1,29 +1,31 @@
 
 
 
-class Persona(val dni: String, cuentas: Array<Cuenta?>) {
+class Persona(val dni: String, private val cuentasIniciales: Array<Cuenta?>) {
 
     init {
         require(dni.length == 9){throw IllegalArgumentException("El DNI debe contener 9 digitos")}
         require(dni[8].isLetter()){throw IllegalArgumentException("El ultimo caracter del DNI debe ser una letra")}
-        require(cuentas.size <= 3){throw IllegalArgumentException("Solo puede haber un maximo de 3 cuentas vinculada a la persona")}
+        require(cuentasIniciales.size <= 3){throw IllegalArgumentException("Solo puede haber un maximo de 3 cuentas vinculada a la persona")}
     }
 
-    constructor(dni: String): this(dni, cuentas = arrayOf(null, null, null)){
+    constructor(dni: String): this(dni, cuentasIniciales = arrayOf(null, null, null)){
 
     }
-
-    var cuentas:Array<Cuenta?> = this.añadirCuentasIniciales()
 
     private fun añadirCuentasIniciales(): Array<Cuenta?>{
         var i = 0
         val cuentasIntroducidas = arrayOf<Cuenta?>(null, null, null)
-        for (cuenta in cuentas){
+        for (cuenta in cuentasIniciales){
             cuentasIntroducidas[i] = cuenta
             i++
         }
         return cuentasIntroducidas
     }
+
+    var cuentas:Array<Cuenta?> = this.añadirCuentasIniciales()
+
+
 
     fun añadirCuenta(cuentaAñadida: Cuenta){
         var i = 0
@@ -34,6 +36,35 @@ class Persona(val dni: String, cuentas: Array<Cuenta?>) {
                 println("Cuenta no añadida, esta persona ya posee 3 cuentas")
             }
             i++
+        }
+    }
+
+    fun esMorosa():Boolean{
+        for (cuenta in cuentas){
+            if (cuenta != null){
+                if ((cuenta.saldo ?: 0.0) < 0) {
+                    println("Es morosa")
+                    return true
+                }
+            }
+        }
+        println("No es morosa")
+        return false
+    }
+
+    fun transferencia(persona1: Persona, persona2: Persona, identificador1 :Int, identificador2: Int, cantidad: Double):Boolean{
+        if (cantidad <= 0.0) { throw IllegalArgumentException("No puedes transferir menos de 0€")}
+        if (persona1.cuentas[identificador1] == null) {throw IllegalArgumentException("No puedes introducir un nulo")}
+        if (persona2.cuentas[identificador2] == null) {throw IllegalArgumentException("No puedes introducir un nulo")}
+
+        if (persona1.cuentas[identificador1]?.saldo?: 0.0 >= cantidad){
+            persona1.cuentas[identificador1]?.realizarPago(cantidad)
+            persona2.cuentas[identificador2]?.recibirAbono(cantidad)
+            println("Transferencia realizada")
+            return true
+        } else {
+            println("No hay suficiente dinero en la cuenta (${persona1.cuentas[identificador1]?.numCuenta}) para realizar la transferencia")
+            return false
         }
     }
 }
