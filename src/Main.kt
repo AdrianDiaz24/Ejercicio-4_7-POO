4aa2e85805ba99e5a6b9770160970e426865156a
AdrianDiaz24 <adiaza@iesrafaelalberti.es>
2025-01-31 20:56:30 +0100

    31/01

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,7 @@
 fun main(){
     val cuenta1 = Cuenta(1)
     val cuenta2 = Cuenta(2, 700.0)
-    val persona1 = Persona("32939158F", arrayOf(cuenta1,cuenta2 ))
+    val persona1 = Persona("32939158F", arrayOf(cuenta1,cuenta2))
     persona1.cuentas[0]?.recibirAbono(1100.0)
     persona1.cuentas[1]?.realizarPago(750.0)
     persona1.esMorosa()
